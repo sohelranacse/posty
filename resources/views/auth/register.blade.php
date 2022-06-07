b45c03f9d142dee2d0d47763c93a090451a41eea
Sohel Rana <37040878+sohelranacse@users.noreply.github.com>
2022-06-07 17:40:16 +0600

    register

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+
+
+@section('content')
+
+<div class="flex justify-center">
+  <div class="w-4/12 bg-white p-6 rounded-lg">
+    <form action="{{ route('register') }}" method="POST">
+
+    </form>
+  </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('hello');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     return view('posts.index');
 });
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
